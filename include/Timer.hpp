24b47f8e93f9593025ee30d9ba7fdd2eb2f10d63
Ananta Krishnan <anantukid@gmail.com>
2026-03-11 22:20:12 +0530

    chore: format

--- include/Timer.hpp
+++ include/Timer.hpp
@@ -4,16 +4,17 @@
 #include <chrono>
 #include <string>
 
-class Timer {
+class Timer
+{
 public:
-    void start();
-    void stop();
-    double elapsed_ms() const;
-    void print(const std::string& label) const;
+  void start();
+  void stop();
+  double elapsed_ms() const;
+  void print(const std::string &label) const;
 
 private:
-    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
-    std::chrono::time_point<std::chrono::high_resolution_clock> end_time;
+  std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+  std::chrono::time_point<std::chrono::high_resolution_clock> end_time;
 };
 
 #endif
